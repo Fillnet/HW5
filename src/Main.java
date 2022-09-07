027346b643a8a6705594a56b8da6504914d044f0
Fillnet <filipp@tenikaytis.ru>
2022-09-08 01:17:39 +0300

    12

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
         task1();
         task2();
         task3();
-        //task4();
+        task4();
         //task5();
         //task6();
         //task7();
@@ -11,64 +13,67 @@ public class Main {
     }
 
     private static void task1() {
-        char color = 'g';
 
-        if (color == 'r') {
-            System.out.println("Красный! Ехать нельзя!");
-        } else if (color == 'y') {
-            System.out.println("Желтый. Внимание.");
-        } else {
-            System.out.println("Зеленый! Проезд разрешен.");
+        int clientOS = 1;
+        if (clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке ");
+        } else if (clientOS == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
         }
     }
+
 
     private static void task2() {
-        int dayOfWeekNumber = 7;
 
-        switch (dayOfWeekNumber) {
-            case 1:
-                System.out.println("Понедельник");
-                break;
-            case 2:
-                System.out.println("Вторник");
-                break;
-            case 3:
-                System.out.println("Среда");
-                break;
-            case 4:
-                System.out.println("Четверг");
-                break;
-            case 5:
-                System.out.println("Пятница");
-                break;
-            case 6:
-                System.out.println("Суббота");
-                break;
-            case 7:
-                System.out.println("Воскресенье");
-                break;
-            default:
-                System.out.println("Такого дня не существует");
+        int clientOS = 1;
+        int clientDeviceYear = 2015;
+        if (clientOS == 0 && clientDeviceYear >= 2015) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (clientOS == 0 && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        }
+        if (clientOS == 1 && clientDeviceYear >= 2015) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        } else if (clientOS == 1 && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
         }
     }
-        private static void task3() {
-            int dayOfWeekNumber = 3;
 
-            switch (dayOfWeekNumber) {
-                case 1:
-                case 2:
-                case 3:
-                case 4:
-                case 5:
-                    System.out.println("Будний день");
-                    break;
-                case 6:
-                case 7:
-                    System.out.println("Выходной день");
-                    break;
-                default:
-                    System.out.println("Такого дня не существует");
-            }
-        }
+    private static void task3() {
+        int year = 2021;
+        boolean leapyear = (year % 400 == 0) || (year % 4 == 0 && year % 100 != 0);
+        if (leapyear)
+            System.out.println("Год " + year + " високосный");
+        else
+            System.out.println("Год " + year + " не високосный");
     }
+
+    private static void task4() {
+
+        //  int deliveryDistance = 95;
+        //  int day = 1;
+        //  if (deliveryDistance > 20) {
+        //     day = (deliveryDistance / 40) + day;
+        //  }
+        //  System.out.println("Потребуется дней : " + day);   было бы интересно понять как сделать через Math.round но пока не знаю как)
+
+        int day, nextDay, deliveryDistance, interval;
+        day = 1;
+        interval = 40;
+        deliveryDistance = 101;
+        nextDay = deliveryDistance / interval;
+        boolean nextDay1 = (nextDay != 0 && nextDay != 1);
+        if (deliveryDistance < 20) {
+            System.out.println(day);
+        }
+        if (nextDay1)
+            nextDay = nextDay + 1;
+        else nextDay = nextDay + 2;
+
+        System.out.println(nextDay);
+
+    }
+}
+
+
 
